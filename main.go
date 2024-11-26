b8d4f311896055990db9feb9c2a41ab533850aa7
rinaldial11 <rinaldilengkana44@gmail.com>
2024-11-26 18:48:28 +0700

    fixing format output

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main ()  {
-	fmt.Println(fmt.Sprintln("Total harga produk Rp.", totaldue.Discount(120000, "50sale")))
-	fmt.Println(fmt.Sprintln("Total ongkos kirim Rp.", totaldue.ShippingCost(3)))
+	shippingCost := totaldue.ShippingCost(3)
+	productCost := totaldue.Discount(120000, "50sale")
+
+	fmt.Println(fmt.Sprintln("Total harga produk = Rp.", productCost) + fmt.Sprintln("Total ongkos kirim = Rp.", shippingCost))
+	fmt.Println("Total yang harus dibayar = Rp.", shippingCost + productCost)
+
 }
